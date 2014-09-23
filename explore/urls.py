91059c5281af0973214859e0358d74cd2c8411c0
Seth Hill <seth@pointnineseven.com>
2014-09-23 09:00:31 -0700

    Use TemplateView rather than the non-existent direct_to_template MP-135

--- explore/urls.py
+++ explore/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.conf.urls import url, patterns
+from django.views.generic.base import TemplateView
 from views import *
 
 urlpatterns = patterns('',
     (r'^catalog', data_catalog),
-    (r'^noaa_catalog', direct_to_template, {'template': 'noaa_catalog.html'}),
+    (r'^noaa_catalog', TemplateView.as_view(), {'template': 'noaa_catalog.html'}),
     (r'^needs', data_needs),
     (r'^resources', external_resources),
     (r'^map_tile_example/([\w-]*)', map_tile_example),
