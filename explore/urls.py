379ac6d09536667ea93620e560ca4c0364c49ee0
Drew <drew@pointnineseven.com>
2015-05-14 16:06:28 -0700

    MP-637 enable ability to go back to previous page

--- explore/urls.py
+++ explore/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     (r'^map_tile_esri_example/([\w-]*)', map_tile_esri_example),
     (r'^map_tile_leaflet_example/([\w-]*)', map_tile_leaflet_example),
     (r'^arcrest_example/([\w-]*)', arcrest_example),
-    (r'^([\w-]*)', tiles_page),
+    (r'^([\w-]*)/', tiles_page),
 )
