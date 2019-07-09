21a7bd9a4ebd25f50054323f9a4f31123ee0b270
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-07-09 15:32:28 -0700

    django 2 url patterns update

--- explore/urls.py
+++ explore/urls.py
@@ -1,13 +1,15 @@
-from django.conf.urls import url, patterns
+# from django.conf.urls import url, patterns
+from django.urls import re_path, include
 from django.conf import settings
 from django.views.generic.base import TemplateView
 from .views import *
 
-urlpatterns = patterns('',
-    (r'^noaa_catalog', TemplateView.as_view(), {'template': 'noaa_catalog.html'}),
-    (r'^map_tile_example/([\w-]*)', map_tile_example),
-    (r'^map_tile_esri_example/([\w-]*)', map_tile_esri_example),
-    (r'^map_tile_leaflet_example/([\w-]*)', map_tile_leaflet_example),
-    (r'^arcrest_example/([\w-]*)', arcrest_example),
-    (r'^([\w-]*)/', tiles_page),
-)
+urlpatterns = [
+    #'',
+    re_path(r'^noaa_catalog', TemplateView.as_view(), {'template': 'noaa_catalog.html'}),
+    re_path(r'^map_tile_example/([\w-]*)', map_tile_example),
+    re_path(r'^map_tile_esri_example/([\w-]*)', map_tile_esri_example),
+    re_path(r'^map_tile_leaflet_example/([\w-]*)', map_tile_leaflet_example),
+    re_path(r'^arcrest_example/([\w-]*)', arcrest_example),
+    re_path(r'^([\w-]*)/', tiles_page),
+]
