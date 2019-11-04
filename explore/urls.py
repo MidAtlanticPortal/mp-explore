0428b8fbc8fce3365916b435200770aadea8628f
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-11-04 13:33:38 -0800

    updating imports for updated django 1.9

--- explore/urls.py
+++ explore/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, patterns
 from django.conf import settings
 from django.views.generic.base import TemplateView
-from views import *
+from .views import *
 
 urlpatterns = patterns('',
     (r'^noaa_catalog', TemplateView.as_view(), {'template': 'noaa_catalog.html'}),
