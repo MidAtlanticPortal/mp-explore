e24504ed9fd06705638e4d57474128cff6f03b1a
Ryan Hodges <ryan.d.hodges@gmail.com>
2020-11-25 15:28:48 -0800

    replacing render_to_request with render to be django 1.10+ compliant

--- explore/views.py
+++ explore/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import get_object_or_404, render
 from django.template import RequestContext
 from data_manager.models import *
 
@@ -34,31 +34,31 @@ def tiles_page(request, slug=None, template='explore/tiles_page.html'):
     arctile_url = orig_url.replace('{z}', '{level}').replace('{x}', '{col}').replace('{y}', '{row}')
     arcrest_url = orig_url.replace('/export', '')
     context = {'layer': layer, 'arctile_url': arctile_url, 'arcrest_url': arcrest_url, 'domain': get_domain(8000)}
-    return render_to_response(template, RequestContext(request, context))
+    return render(request, template, context)
 
 def map_tile_example(request, slug=None, template='explore/map_tile_example.html'):
     layer = get_object_or_404(Layer, slug_name=slug)
     context = {'layer': layer}
-    return render_to_response(template, RequestContext(request, context))
+    return render(request, template, context)
 
 def map_tile_esri_example(request, slug=None, template='explore/map_tile_esri_example.html'):
     layer = get_object_or_404(Layer, slug_name=slug)
     orig_url = layer.url
     arctile_url = orig_url.replace('{z}', '{level}').replace('{x}', '{col}').replace('{y}', '{row}')
     context = {'layer': layer, 'arctile_url': arctile_url}
-    return render_to_response(template, RequestContext(request, context))
+    return render(request, template, context)
 
 def map_tile_leaflet_example(request, slug=None, template='explore/map_tile_leaflet_example.html'):
     layer = get_object_or_404(Layer, slug_name=slug)
     orig_url = layer.url
     leaflet_url = orig_url.replace('$', '')
     context = {'layer': layer, 'leaflet_url': leaflet_url}
-    return render_to_response(template, RequestContext(request, context))
+    return render(request, template, context)
 
 def arcrest_example(request, slug=None, template='explore/arcrest_example.html'):
     layer = get_object_or_404(Layer, slug_name=slug)
     context = {'layer': layer}
-    return render_to_response(template, RequestContext(request, context))
+    return render(request, template, context)
 
 def linkify(text):
     return text.lower().replace(' ', '-')
